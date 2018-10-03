384ba727ac41ffc1079636523cabd0cf1c31068b
Mike James <mdacampora@gmail.com>
2018-10-03 19:30:51 -0400

    improved doc

--- train_model.py
+++ train_model.py
@@ -3,46 +3,32 @@ import numpy as np
 import pandas as pd 
 
 # Model algorithm
+import sklearn
 from sklearn.ensemble import RandomForestClassifier
 #
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import Imputer, LabelEncoder
 from sklearn.pipeline import Pipeline
 
 
 # pull and check data
-import pull_data as pull
+# import pull_data as pull
 
 
 def main():
     
     # TURN ON LATER
-    train_set, test_set = pull.pull_data()
+    # train_set, test_set = pull.pull_data()
 
-    # train_set = pd.read_csv("train.csv")
-    # test_set = pd.read_csv("test.csv")
+    train_set = pd.read_csv("train.csv")
+    test_set = pd.read_csv("test.csv")
 
     
     full_data = pd.concat([train_set, test_set], sort=True)
     
     data_cleaner = [train_set, test_set]
 
-
-    
-    train_set, test_set = impute_and_drop(train_set, test_set)
-
-
-
-
-# I have chosen to impute the median in the Age column, Mode in Embarked, and the median in Fare
-# Age had a fairly large left skew so I thought it best to use the median CHECK
-# For Embarked, there were only three ports the titanic left from, only two missing values, and most 
-# were out of one port.
-# The median was chosen also to impute over Fare.
-# Lastly Cabin, PassengerId, and Ticket were dropped, since they dont appear to provide much value    
-def impute_and_drop(train_set, test_set):
-    
     print("")
     print("Checking for null values: train & test")
     print("=" * 20)
@@ -51,6 +37,28 @@ def impute_and_drop(train_set, test_set):
     print(test_set.isnull().sum())
     print("=" * 20)
     
+    train_set, test_set = impute_and_drop(train_set, test_set)
+
+
+
+
+ 
+def impute_and_drop(train_set, test_set):
+    """Impute and drop relevant variables
+    
+    Parameters
+    ----------
+    train_set : DataFrame of the training set
+    test_set : DataFrame of the test set
+        Impute the median for NaNs in the Age column, mode in the Embarked
+        column, and median in the Fare column.
+        Drop Cabin, PassengerId, and Ticket columns in the training set.
+        
+    Returns
+    -------
+    train_set : Imputed DataFrame of the training set
+    test_set : Imputed DataFrame of the test set
+    """  
     for dataset in [train_set, test_set]:
         
         dataset['Age']      = lambda x: x['Age'].fillna(x['Age'].median(), inplace = True)
@@ -70,6 +78,46 @@ def impute_and_drop(train_set, test_set):
     
     return train_set, test_set
 
+
+
+def feature_engineer(train_set, test_set):
+
+    for dataset in [train_set, test_set]:
+
+        pass
+
+
+def create_dummy_variables(train_set, test_set):
+    label = LabelEncoder
+    label.fit_transform
+    for dataset in [train_set, test_set]:
+
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # imp = Imputer(missing_values='Nan', strategy='mean', axis=0)
 
     # clf = SVC()
@@ -82,12 +130,6 @@ def impute_and_drop(train_set, test_set):
     # X_train, X_test, y_train, y_test = train_test_split()
 
     # pipeline.fit(X_train,  y_train)
-
-    
-
-
-
-
 
 
 
